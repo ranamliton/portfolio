b1118df8d3740e5ab7027abbf3f172f38a7ac71b
Liton Rana <37443312+ranalitton@users.noreply.github.com>
2020-12-18 23:44:31 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Researcher", "Student"],
+        strings: ["Student", "Researcher", "Dreamer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
